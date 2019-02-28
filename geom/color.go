a3e3364b994b67be4c047c6abd6b7d518f25ec4f
Ajeet D'Souza <98ajeet@gmail.com>
2019-03-01 00:21:31 +0530

    Add color

---
+++ geom/color.go
@@ -0,0 +1,49 @@
+package geom
+
+import (
+	"gonum.org/v1/gonum/mat"
+)
+
+type Color struct {
+	data mat.Vector
+}
+
+func NewColor(r, g, b float64) Color {
+	return Color{mat.NewVecDense(3, []float64{r, g, b})}
+}
+
+func (color Color) R() float64 {
+	return color.data.AtVec(0)
+}
+
+func (color Color) G() float64 {
+	return color.data.AtVec(1)
+}
+
+func (color Color) B() float64 {
+	return color.data.AtVec(2)
+}
+
+func (color Color) Add(other Color) Color {
+	var result mat.VecDense
+	result.AddVec(color.data, other.data)
+	return Color{&result}
+}
+
+func (color Color) Sub(other Color) Color {
+	var result mat.VecDense
+	result.SubVec(color.data, other.data)
+	return Color{&result}
+}
+
+func (color Color) MulColor(other Color) Color {
+	var result mat.VecDense
+	result.MulElemVec(color.data, other.data)
+	return Color{&result}
+}
+
+func (color Color) MulScalar(k float64) Color {
+	var result mat.VecDense
+	result.ScaleVec(k, color.data)
+	return Color{&result}
+}
